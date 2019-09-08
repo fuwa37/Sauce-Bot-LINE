f5ee02c2f4ddca27cb937177cd7bdf118ff34557
irs37 <irfanreynaldi37@gmail.com>
2019-09-08 15:41:13 +0700

    Delete tes2.py

--- tes2.py
+++
@@ -1,7 +0,0 @@
-import handlers.sauce.get_source as gs
-import handlers.sauce.comment_builder as bc
-
-a = gs.get_source_data("https://res.cloudinary.com/fuwa/image/upload/v1567928580/U2e91a820fc12fa78c55ded8731cd3698.jpg", trace=True)
-a = bc.build_comment(a)
-
-print(a)
