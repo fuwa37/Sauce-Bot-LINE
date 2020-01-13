90e3e64a3f2c990e70c47706a0a014f7f85e44fd
irs37 <irfanreynaldi37@gmail.com>
2020-01-13 01:15:03 +0000

    host

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ port = int(os.environ.get('PORT', 33507))
 
 app.register_blueprint(line)
 
-app.run(host='0.0.0.0', port=port)
+@app.route('/')
+def home():
+    print("Hello")
+    return "Hello"
+
+app.run(host='0.0.0.0',port=8080)
